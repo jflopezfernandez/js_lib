aa28228bdc86879892bf0fe13a4acc71a46550c3
lopezfjose <jflopezfernandez@gmail.com>
2017-06-13 08:36:15 -0400

    Initial commit

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
-document.writeln("testing...");
+
+var gcd = function (m,n) {
+    var r = m % n;
+
+    while (r != 0) {
+        m = n;
+        n = r;
+
+        r = m % n;
+    }
+
+    return n;
+};
+
+function printGCD(m,n) {
+    document.writeln("gcd(", m, ",", n, ") = ", gcd(m,n), "<br />");
+};
+
+function GCD() {
+    var m = prompt("Please enter value for m: ");
+    var n = prompt("Please enter value for n: ");
+
+    printGCD(m,n);
+}
+
+GCD();
